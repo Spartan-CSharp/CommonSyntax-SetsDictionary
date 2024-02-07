b6c3593c7ea7690914645fe3dd4535d0f3bd5dde
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 14:07:59 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main()
 		{
 			Dictionary<int, string> employees = new Dictionary<int, string>
 			{
