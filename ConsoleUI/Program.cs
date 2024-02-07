8e75b660aeb5239bbef282428380c50942bdee49
Pierre Plourde <pierre@spartancsharp.net>
2024-02-07 14:06:47 -0500

    Create Code
    Created a Dictionary list of employee IDs and the name that goes with that ID. Filled it with a few records. Then asked the user for their ID and returned their name.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
+			Dictionary<int, string> employees = new Dictionary<int, string>
+			{
+				[1] = "Tim",
+				[2] = "Sue",
+				[3] = "Bob"
+			};
+
+			Console.Write("Enter your Employee ID: ");
+			string employeeidtext = Console.ReadLine();
+			bool isidvalid = int.TryParse(employeeidtext, out int employeeid);
+			if ( isidvalid && employeeid > 0 && employeeid < 4 )
+			{
+				Console.WriteLine($"Welcome {employees[employeeid]}!");
+			}
+			else
+			{
+				Console.WriteLine("That is not a valid ID.");
+			}
+
+			_ = Console.ReadLine();
 		}
 	}
 }
